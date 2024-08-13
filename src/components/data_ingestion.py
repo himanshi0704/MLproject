9a171260e778f636fffaff68eec0b8453a982604
himanshi0704 <himanshi0710hans@gmail.com>
2024-08-13 17:01:11 +0530

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,12 +1,18 @@
 import os
 import sys
 
+# from pathlib import Path
+# sys.path.append(str(Path(__file__).parent.parent))
+
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass # this is to get free of __init__ in a class to initialize class variables
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig :
@@ -46,7 +52,10 @@ class DataIngestion :
 
 if __name__ == '__main__' :
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data , test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
     
 
